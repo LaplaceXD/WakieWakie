d912aadd9f26876c8cf1c5d0350726bf81063d2a
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-12-17 17:52:05 +0800

    feat(db):lex - added cors, compress, logger, and helmet middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,16 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { compress } from "hono/compress";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { secureHeaders } from "hono/secure-headers";
 
 const app = new Hono().basePath("api/v1");
+
+app.use("*", cors());
+app.use("*", logger());
+app.use("*", compress());
+app.use("*", secureHeaders());
 
 app.get("/ping", async c => {
   return c.json({ message: "PONG!" });
