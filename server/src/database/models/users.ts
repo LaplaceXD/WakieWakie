518dc72867cd8c5dc6238bafef029dfac1626a03
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-12-19 21:11:37 +0800

    feat(db)-lex: created user model

--- server/src/database/models/users.ts
+++ server/src/database/models/users.ts
@@ -1,18 +1,28 @@
+import { randomUUID } from "crypto";
+import { sql } from "drizzle-orm";
 import { index, int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable(
   "users",
   {
-    id: int("id", { mode: "number" }).notNull().primaryKey({ autoIncrement: true }),
-    email: text("email").unique("unique_email").notNull(),
-    username: text("username").notNull().unique("unique_username"),
-    password: text("password").notNull(),
-    status: text("status", { enum: ["active", "deactivated", "deleted", "blocked"] })
-      .default("active")
-      .notNull(),
-    lastLogin: int("last_login", { mode: "timestamp" }),
+    id: text("user_id").primaryKey().notNull().$defaultFn(randomUUID),
+    firstName: text("first_name").notNull(),
+    lastName: text("first_name").notNull(),
+    bio: text("bio").notNull().default(""),
+    gender: text("gender").notNull(),
+    interest: text("interest", { enum: ["Male", "Female", "Both"] }).notNull(),
+    city: text("city").notNull().default(""),
+    country: text("country").notNull(),
+    alarmTime: text("alarm_time").notNull(),
+    createdAt: int("created_at", { mode: "timestamp" })
+      .notNull()
+      .default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: int("updated_at", { mode: "timestamp" }),
   },
-  users => ({
-    usernameIdx: index("username_idx").on(users.username),
+  user => ({
+    genderIdx: index("gender_idx").on(user.gender),
+    interestIdx: index("interest_idx").on(user.interest),
+    alarmTimeIdx: index("alarm_time_idx").on(user.alarmTime),
+    locationIdx: index("location_idx").on(user.city, user.country),
   }),
 );
