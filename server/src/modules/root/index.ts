bf8e0ed22b4daad361ecdee2a88fbef56cb19639
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-12-21 23:44:33 +0800

    feat(server)-lex: added a way to ping server for connection

--- server/src/modules/root/index.ts
+++ server/src/modules/root/index.ts
@@ -1,3 +1,4 @@
 export * as errors from "./root.errors";
+export * from "./root.query";
 export * from "./root.resolvers";
 export * from "./root.schema";
---
+++ server/src/modules/root/root.query.ts
@@ -0,0 +1,5 @@
+import { QueryResolvers } from "@/__generated__/gql";
+
+export const queries: QueryResolvers = {
+  ping: () => "PONG!",
+};
