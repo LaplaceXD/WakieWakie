4f4a8cb9c74fc2fec591e3c63e95edecb5499b6a
jaosherlyr <jaosherlyr@gmail.com>
2023-12-21 22:48:46 +0800

    feat(navbar)-sherly: created routing and navbar

---
+++ client/src/components/analogClock.tsx
@@ -0,0 +1,13 @@
+const AnalogClock = () => {
+  return (
+    <div className="aspect-square w-1/3 rounded-full bg-slate-500">
+      <div>
+        <div id="secondHand"></div>
+        <div id="minuteHand"></div>
+        <div id="hourHand"></div>
+      </div>
+    </div>
+  );
+};
+
+export default AnalogClock;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
+import App from "./App.tsx";
+import ErrorPage from "./pages/errorPage";
+import Messages from "./pages/messages";
+import AnalogClock from "./components/analogClock";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <AnalogClock />,
+      },
+      {
+        path: "/messages",
+        element: <Messages />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
